c58b6ca92f68157d3a52012e63613a447aa8f955
Anuja6125 <raneanuja05@gmail.com>
2026-01-08 16:37:35 +0530

    Added js files

---
+++ JS/bookstore.js
@@ -0,0 +1,22 @@
+let inventory = [
+    {
+        id:1,
+        title: "JS",
+        author: "Author A",
+        price: 30,
+        inStock: true
+    },
+    {
+        id:2,
+        title:"Python",
+        author: "Author B",
+        price: 25,
+        inStock: false
+    },
+    {
+        id:3,
+        title:"Java",
+        price: 40,
+        inStock: true
+    }
+]
