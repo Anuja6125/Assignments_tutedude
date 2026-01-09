47ae8d939bf7c0db2bacba038997c5f426af956c
Anuja6125 <raneanuja05@gmail.com>
2026-01-09 20:53:36 +0530

    Added required fields to laundry mart

--- JS/bookstore.js
+++ JS/bookstore.js
@@ -18,5 +18,31 @@ let inventory = [
         title:"Java",
         price: 40,
         inStock: true
-    }
-]
+    },
+
+    addBooks: function (book) {
+        inventory.push(book);
+   }
+];
+
+const listBooks = () => {
+    return inventory.map(book => book.title);
+}
+
+const getAvailableBooks = () => {
+    return inventory.filter(book => book.inStock);
+}
+
+const sellbook = (bookId) => {
+    const book = inventory.find(b => b.id === bookId);
+    if (book && book.inStock) {
+        book.inStock = false;
+        return `Sold: ${book.title}`;
+    } else {
+        return `Book not available`;
+    }   
+}
+
+const totalprice = () => {
+    return inventory.reduce((total, book) => total + book.price, 0);
+}
